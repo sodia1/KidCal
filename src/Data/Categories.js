641fab385009331c358e2df6a81ae2a376b231f1
sodia1 <gauravsodia@gmail.com>
2020-03-06 20:17:45 +0530

    First Commit

---
+++ src/Data/Categories.js
@@ -0,0 +1,24 @@
+const Categories = [
+    {
+        text: "Addition",
+        uri: "/add"
+    },
+    {
+        text: "Subtraction",
+        uri: "/sub"
+    },
+    {
+        text: "Multiply",
+        uri: "/multiply"
+    },
+    {
+        text: "Comparison",
+        uri: "/comparison"
+    },
+    // {
+    //     text: "Division",
+    //     uri: "/division"
+    // }
+]
+
+export default Categories
---
+++ src/Helpers/Number/Random.js
@@ -0,0 +1,17 @@
+const Random = (min, max, frequency = 2, operator) => {
+    let results = [];
+    while(frequency > 0) {
+        let num = Math.round(Math.random()* (max-min) + min);
+        if(operator === '___'){
+            results.push(num)
+            --frequency
+        }
+        else if(! results.includes(num)){
+            results.push(num)
+            --frequency
+        }
+    }
+    return results;
+}
+
+export default Random;
